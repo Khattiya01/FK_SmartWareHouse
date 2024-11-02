0afc0a2ae024f5058e9478c8d8cbb018af36ccd7
Khattiya <khattiya.dokbua@gmail.com>
2024-11-02 22:39:34 +0700

    update

--- src/middleware/isAuthenticated.ts
+++ src/middleware/isAuthenticated.ts
@@ -3,7 +3,6 @@ import { NextRequest } from "next/server";
 
 export const isAuthenticated = (req: NextRequest) => {
   const token = req.headers.get("Authorization")?.split(" ")[1];
-  console.log("token isAuthenticated", token);
   if (!token) {
     return false;
   }
